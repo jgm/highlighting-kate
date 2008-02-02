69fc129f03033ba3be872b37c65da2bffe8a8d83
jgm@berkeley.edu <unknown>
2008-02-02 22:25:34 +0000

    Export definitions in Text.Highlighting.Kate.Definitions. Definitions used only in ParseSyntaxFiles.hs are moved to that file. Haddock comments added, plus a note in README about how to generate docs.

--- ParseSyntaxFiles.hs
+++ ParseSyntaxFiles.hs
@@ -38,6 +38,52 @@ import Text.PrettyPrint
 import Text.Highlighting.Kate.Definitions
 import Text.Highlighting.Kate.Common (capitalize)
 
+data SyntaxDefinition =
+  SyntaxDefinition { synLanguage      :: String
+                   , synAuthor        :: String
+                   , synVersion       :: String
+                   , synLicense       :: String
+                   , synExtensions    :: String
+                   , synCaseSensitive :: Bool
+                   , synLists         :: [(String, [String])]
+                   , synContexts      :: [SyntaxContext]
+                   , synItemDatas     :: [(String, String)]
+                   , synKeywordAttr   :: SyntaxKeywordAttr
+                   } deriving (Read, Show)
+
+data SyntaxKeywordAttr =
+  SyntaxKeywordAttr  { keywordCaseSensitive   :: Bool
+                     , keywordWeakDelim       :: [Char]
+                     , keywordAdditionalDelim :: [Char]
+                     } deriving (Read, Show)
+
+data SyntaxContext = 
+  SyntaxContext { contName               :: String
+                , contAttribute          :: String
+                , contLineEndContext     :: String
+                , contLineBeginContext   :: String
+                , contFallthrough        :: Bool
+                , contFallthroughContext :: String
+                , contDynamic            :: Bool
+                , contParsers            :: [SyntaxParser] 
+                } deriving (Read, Show)
+
+data SyntaxParser =
+  SyntaxParser { parserType              :: String
+               , parserAttribute         :: String
+               , parserContext           :: String
+               , parserLookAhead         :: Bool
+               , parserIncludeAttrib     :: Bool
+               , parserFirstNonSpace     :: Bool
+               , parserColumn            :: Maybe Int
+               , parserDynamic           :: Bool
+               , parserString            :: String -- could be a regex
+               , parserChar              :: Char
+               , parserChar1             :: Char
+               , parserChildren          :: [SyntaxParser]
+               } deriving (Read, Show)
+
+
 libraryPath = joinPath ["Text", "Highlighting", "Kate"]
 destDir = joinPath [libraryPath, "Syntax"]
 
--- Text/Highlighting/Kate/Definitions.hs
+++ Text/Highlighting/Kate/Definitions.hs
@@ -1,64 +1,24 @@
 module Text.Highlighting.Kate.Definitions where
 import qualified Data.Map as Map
 
-data SyntaxDefinition =
-  SyntaxDefinition { synLanguage      :: String
-                   , synAuthor        :: String
-                   , synVersion       :: String
-                   , synLicense       :: String
-                   , synExtensions    :: String
-                   , synCaseSensitive :: Bool
-                   , synLists         :: [(String, [String])]
-                   , synContexts      :: [SyntaxContext]
-                   , synItemDatas     :: [(String, String)]
-                   , synKeywordAttr   :: SyntaxKeywordAttr
-                   } deriving (Read, Show)
-
-data SyntaxKeywordAttr =
-  SyntaxKeywordAttr  { keywordCaseSensitive   :: Bool
-                     , keywordWeakDelim       :: [Char]
-                     , keywordAdditionalDelim :: [Char]
-                     } deriving (Read, Show)
-
-data SyntaxContext = 
-  SyntaxContext { contName               :: String
-                , contAttribute          :: String
-                , contLineEndContext     :: String
-                , contLineBeginContext   :: String
-                , contFallthrough        :: Bool
-                , contFallthroughContext :: String
-                , contDynamic            :: Bool
-                , contParsers            :: [SyntaxParser] 
-                } deriving (Read, Show)
-
-data SyntaxParser =
-  SyntaxParser { parserType              :: String
-               , parserAttribute         :: String
-               , parserContext           :: String
-               , parserLookAhead         :: Bool
-               , parserIncludeAttrib     :: Bool
-               , parserFirstNonSpace     :: Bool
-               , parserColumn            :: Maybe Int
-               , parserDynamic           :: Bool
-               , parserString            :: String -- could be a regex
-               , parserChar              :: Char
-               , parserChar1             :: Char
-               , parserChildren          :: [SyntaxParser]
-               } deriving (Read, Show)
-
+-- | A stack of context names for each language.  (Language-specific context
+-- stacks must be maintained because of IncludeRules.)
 type ContextStack = Map.Map String [String]
 
-data SyntaxState = SyntaxState { synStContexts             :: ContextStack
-                               , synStLanguage             :: String
-                               , synStCurrentLine          :: String 
-                               , synStCharsParsedInLine    :: Int
-                               , synStCaseSensitive        :: Bool
-                               , synStKeywordCaseSensitive :: Bool
-                               , synStKeywordDelims        :: [Char]
-                               , synStCaptures             :: [String]
+-- | State for syntax parser.
+data SyntaxState = SyntaxState { synStContexts             :: ContextStack -- ^ Stack of contexts (top is current)
+                               , synStLanguage             :: String       -- ^ Language being parsed 
+                               , synStCurrentLine          :: String       -- ^ Contents of current line
+                               , synStCharsParsedInLine    :: Int          -- ^ Number of characters parsed in line so far
+                               , synStCaseSensitive        :: Bool         -- ^ True if language is case-sensitive
+                               , synStKeywordCaseSensitive :: Bool         -- ^ True if keywords are case-sensitive
+                               , synStKeywordDelims        :: [Char]       -- ^ List of keyword delimiters
+                               , synStCaptures             :: [String]     -- ^ List of regex captures from last capturing match
                                } deriving (Read, Show)
 
+-- | A pair consisting of a list of attributes and some text.
 type LabeledSource = ([String], String)
 
+-- | A line of source, including a line number and a list of labeled source items.
 data SourceLine = SourceLine Int [LabeledSource] deriving (Read, Show, Eq)
 
