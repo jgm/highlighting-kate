97af52cabef5e85d02b1348a05e795112d2761a5
John MacFarlane <fiddlosopher@gmail.com>
2011-12-22 23:00:48 -0800

    Comment improvements.

--- Text/Highlighting/Kate.hs
+++ Text/Highlighting/Kate.hs
@@ -1,23 +1,25 @@
 {- |
    Module      : Text.Highlighting.Kate
-   Copyright   : Copyright (C) 2008 John MacFarlane
-   License     : GNU GPL, version 2 or above 
+   Copyright   : Copyright (C) 2008-2011 John MacFarlane
+   License     : GNU GPL, version 2 or above
 
    Maintainer  : John MacFarlane <jgm@berkeley.edu>
-   Stability   : alpha 
+   Stability   : alpha
    Portability : portable
 
 This helper module exports the main highlighting and formatting
 functions.
-  
+
 A typical application will combine a highlighter and a formatter:
 
+> import Text.Highlighting.Kate
+> import Text.Blaze.Renderer.String
+>
 > main = do
 >   code <- getContents
->   case highlightAs "ruby" code of
->         Right result -> putStrLn $ renderHtmlFragment $ 
->                         formatAsHtml [OptNumberLines] "ruby" result
->         Left  err    -> error $ "Could not parse input: " ++ err
+>   putStrLn $ renderHtml
+>            $ formatAsHtml [OptNumberLines] "ruby"
+>            $ highlightAs "ruby" code
 
 -}
 
